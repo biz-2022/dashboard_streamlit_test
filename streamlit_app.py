dfeafa4619ed248c4df1e0b0b9022328554dfa5b
biz-2022 <121501370+biz-2022@users.noreply.github.com>
2022-12-27 23:19:19 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 def main():
     st.title("Counter Example")
-    if "count" not in st.session_state:  # (C)
-        st.session_state.count = 0  # (A)
+    # if "count" not in st.session_state:  # (C)
+    #     st.session_state.count = 0  # (A)
 
-    increment = st.button("Increment")  # (B)
-    if increment:
-        st.session_state.count += 1  # (A)
+    # increment = st.button("Increment")  # (B)
+    # if increment:
+    #     st.session_state.count += 1  # (A)
 
-    st.write("Count = ", st.session_state.count)  # (A)
+    # st.write("Count = ", st.session_state.count)  # (A)
 
 
 if __name__ == "__main__":
